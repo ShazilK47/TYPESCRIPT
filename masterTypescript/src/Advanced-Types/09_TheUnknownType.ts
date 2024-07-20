a31fa1d266de2e3684ea8ce5ccdb9b1b8c168e01
ShazilK47 <shazil.akn@gmail.com>
2024-07-21 00:52:22 +0500

    The unkown Type in TS

---
+++ masterTypescript/src/Advanced-Types/09_TheUnknownType.ts
@@ -0,0 +1,8 @@
+// # The unknown type
+// function render(document: unknown) {
+// // We have to narrow down to a specific
+// // type before we can perform any operations
+// // on an unknown type.
+// if (typeof document === 'string') {
+// }
+// }
