ee9fb99b7b0a47fc273cd79a71aa12e203ff5697
ShazilK47 <shazil.akn@gmail.com>
2024-07-21 00:52:01 +0500

    Type Assertion in TS

---
+++ masterTypescript/src/Advanced-Types/08_typeAssertion.ts
@@ -0,0 +1,4 @@
+//Type Assertion
+let phone = document.getElementById("phone") as HTMLInputElement; //type assertion : telling the compiler about the type of the object.
+//let phone = <HTMLInputElement> document.getElementById('phone');
+phone.value;
