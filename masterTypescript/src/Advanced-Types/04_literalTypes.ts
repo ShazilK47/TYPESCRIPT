e8660b13d5ff0cfa69e0e5da11922a33a3f25a1d
ShazilK47 <shazil.akn@gmail.com>
2024-07-21 00:50:33 +0500

    Literals Types in TS

---
+++ masterTypescript/src/Advanced-Types/04_literalTypes.ts
@@ -0,0 +1,9 @@
+// # Literal TypeS
+//Literal(exact, specific)
+//to limit the values that are assigned to a variable>
+
+type Quantity = 50 | 100; //here type literal in action.
+let Quantity: Quantity = 50; // now it can be 50 or 100 but nothing else.
+
+//another example
+type Metric = "cm" | "inch";
